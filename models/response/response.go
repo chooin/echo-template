5edf38fc88b2ddccccbeb4666d6389b166dd6f32
Devin.Zhu <chooin@126.com>
2024-08-03 15:36:11 +0800

    refactor: Refactor response structs for consistency

--- models/response/response.go
+++ models/response/response.go
@@ -7,48 +7,41 @@ import (
 
 type Data = map[string]interface{}
 
-type Error struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
-}
-
 type Response struct {
 	Message string `json:"message"`
 	Data    any    `json:"data"`
 }
 
-type ExceptionResponse struct {
-	Message string  `json:"message"`
-	Errors  []Error `json:"errors"`
+type Error struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
 }
 
 func Ok(c echo.Context, data any, code ...int) error {
 	httpStatus := http.StatusOK
+
 	if len(code) > 0 && code[0] >= 200 && code[0] < 300 {
 		httpStatus = code[0]
 	}
+
 	return c.JSON(httpStatus, data)
 }
 
-// ClientException 客户端异常
-func ClientException(c echo.Context, message string, errors []Error, code ...int) error {
+func ClientException(c echo.Context, message string, data any, code ...int) error {
 	httpStatus := http.StatusBadRequest
+
 	if len(code) > 0 && code[0] >= 400 && code[0] < 500 {
 		httpStatus = code[0]
 	}
-	if errors == nil {
-		errors = []Error{}
-	}
-	return c.JSON(httpStatus, &ExceptionResponse{
+
+	return c.JSON(httpStatus, &Response{
 		Message: message,
-		Errors:  errors,
+		Data:    data,
 	})
 }
 
-// ServerException 服务端异常
 func ServerException(c echo.Context, message string) error {
-	return c.JSON(http.StatusInternalServerError, &ExceptionResponse{
-		Message: message,
-		Errors:  []Error{},
+	return c.JSON(http.StatusInternalServerError, Data{
+		"message": message,
 	})
 }
